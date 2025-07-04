a082e7a33f8ffd803e4cce9390607e50da5c1631
Nikita <117333817+SanctusBebrus@users.noreply.github.com>
2025-07-04 18:05:18 +1000

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,36 +1,34 @@
+<?php
+if (!isset($_COOKIE['User'])) {
+    header('Location: login.php');
+    exit;
+}
+
+$user = htmlspecialchars($_COOKIE['User']);
+?>
+
 <!DOCTYPE html>
 <html lang="ru">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
     <title>KUDRYAVTSEV</title>
 </head>
 <body>
-<div class="container">
-    <div class="row">
-        <div class="col-8">
-            <img src="https://static.wikia.nocookie.net/azumanga/images/8/8a/America_Ya.jpg"
-                 alt="somethingfunny">
-        </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <h1 class="hello">
-                        Привет, <?php echo $_COOKIE['User']; ?>
-                    </h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-8">
+                <img src="https://static.wikia.nocookie.net/azumanga/images/8/8a/America_Ya.jpg" alt="somethingfunny">
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h1 class="hello">
+                            Привет, <?php echo $user; ?>
+                        </h1>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 </body>
 </html>
-
-<?php
-require_once('./db.php');
-
-$link = mysqli_connect('127.0.0.1', 'root', 'qwe', 'first_db');
-
-    mysqli_close($link);
-
-?>
