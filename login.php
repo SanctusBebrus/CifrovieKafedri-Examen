523d6af6ea0c08aa00ee0ad8674efd925b07cac4
Nikita <117333817+SanctusBebrus@users.noreply.github.com>
2025-07-04 18:04:34 +1000

    Update login.php

--- login.php
+++ login.php
@@ -1,52 +1,50 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>KUDRYAVTSEV</title>
-</head>
-<body>
-    <div class="container">
-        <div class="row">
-            <div class="col-12 index">
-                <h1>Вход</h1>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-12">
-                <form method='POST' action='/login.php'>
-                    <div class="row from_reg"><input class="from" type="text" name="login" placeholder="Login"></div>
-                    <div class="row from_reg"><input class="from" type="password" name="password" placeholder="Password"></div>
-                    <button type='submit' class="btn_reg" name="submit">Продолжить</button>
-                </form>
-            </div>
-        </div>
-    </div>
-</body>
-</html> 
-
 <?php
 require_once('./db.php');
-$link = mysqli_connect('127.0.0.1', 'root', 'qwe', 'first_db');
+$link = mysqli_connect('db', 'root', 'qwe', 'first_db');
+
+session_start();
+
+$error = '';
 
 if (isset($_POST['submit'])) 
 {
-    $username = $_POST['login'];
-    $password = $_POST['password'];
+    $username = $_POST['login'] ?? '';
+    $password = $_POST['password'] ?? '';
 
-    if (!$username || !$password) die ('Заполните все поля');
+    if (!$username || !$password) {
+        $error = 'Заполните все поля';
+    } else {
+        $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+        $result = mysqli_query($link, $sql);
 
-    $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
-
-    $result = mysqli_query($link, $sql);
-
-    if (mysqli_num_rows($result) == 1) 
-    {
-        setcookie("User", $username, time()+7200);
-        header('Location: profile.php');
-    } else 
-    {
-        echo "Неверное имя или пароль";
+        if (mysqli_num_rows($result) == 1) {
+            setcookie("User", $username, time() + 7200, "/");
+            header('Location: profile.php');
+            exit;
+        } else {
+            $error = "Неверное имя или пароль";
+        }
     }
 }
+
+mysqli_close($link);
 ?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8" />
+    <title>KUDRYAVTSEV</title>
+</head>
+<body>
+    <h1>Вход</h1>
+    <?php if ($error): ?>
+        <p style="color:red;"><?php echo htmlspecialchars($error); ?></p>
+    <?php endif; ?>
+    <form method="POST" action="">
+        <input type="text" name="login" placeholder="Login" required><br>
+        <input type="password" name="password" placeholder="Password" required><br>
+        <button type="submit" name="submit">Продолжить</button>
+    </form>
+</body>
+</html>
